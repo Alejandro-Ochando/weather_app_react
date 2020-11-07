d493aac7a1358db9bbc6f7ee4607b2e401a3b9f3
Alejandro-Ochando <alejandro.ochando@live.com>
2020-11-07 03:27:06 +0100

    Creando la validación y  pasando el codigo y nombre de la ciudad a APP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Buscador from './componets/Buscador';
 
 
-
-
 function App() {
+
+  const [ city, saveCity ] = useState('');
+  const [ code, saveCode ] = useState(0);
+
   return (
     <Fragment>
       <div className="container">
-        <Buscador />
+        <Buscador 
+          saveCity={saveCity}
+          saveCode={saveCode}
+        />
       </div>
     </Fragment>
   );
--- src/componets/Buscador.js
+++ src/componets/Buscador.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '../data/pueblos.json';
 
-
-const Buscador = () => {
-
+const Buscador = ({ saveCity, saveCode }) => {
     const element = <FontAwesomeIcon icon={faSearch} />
+    
+    const [ search, saveSearch ] = useState('');
+
+    const handlerChange = e => {
+        saveSearch(e.target.value);
+    }
+
+    const handlerSubmit = e => {
+        e.preventDefault();
+
+        const city = search.toLowerCase();
+        const datacity = require('../data/pueblos.json');
+        
+        if(city.trim() === '' || isNaN(city) === false){
+            console.log("error");
+            return;
+        }
+
+        const result = datacity.filter(pueblos => city === pueblos.name);
+        
+        if( result.length >= 1 ){
+            saveCity(result[0].name);
+            saveCode(result[0].id);
+        }else{
+            console.log("error");
+            saveCity('');
+            saveCode('');
+            return;
+        }
+    }
 
     return ( 
         
         <div className="container center">
-            <div class="row">
-                <form class="col s12">
-                    <div class="row">
-                        <div class="col s12">
+            <div className="row">
+                <form 
+                    className="col s12"
+                    autoComplete="off"
+                    onSubmit={handlerSubmit}
+                >
+                    <div className="row">
+                        <div className="col s12">
                             <p className="location-text">Localización:</p>
-                            <div class="input-field inline">
-                                <input type="text"/>
+                            <div className="input-field inline">
+                                <input 
+                                    type="text"
+                                    name="city"
+                                    onChange={handlerChange}
+                                />
                                 <label className="location-label">{element}</label>
                             </div>
                         </div>

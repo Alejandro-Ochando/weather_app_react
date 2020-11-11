2271852cb1cb6159f5e6b5e905b500ff2b81d8af
Alejandro-Ochando <alejandro.ochando@live.com>
2020-11-11 22:00:39 +0100

    Añadiendo iconos del viento, ajustando la resolución de pantalla y más css

--- src/componets/Hours.js
+++ src/componets/Hours.js
@@ -4,23 +4,27 @@ import './Hours.css';
 
 
 const Hours = ({ days }) => {
-    console.log();
     if(days.length === 0 || days.length === '') return null;
-    const prueba= Object.values(days).slice(0, 6);
+    const screen = (window.screen.width);
     
-    /* */
+    let  sixDays = Object.values(days).slice(0, 6);
+    let tamañoDias = 6;
+    if(screen < 900 ){
+        tamañoDias = 4;
+        sixDays = Object.values(days).slice(0, 4);
+    }
+   
     return ( 
-        <div className="container mt-2 center">
+        <div className="container glass center">
             <div className="row m12" >
-                
-                    {Object.values(prueba).map( day => (
+                    {Object.values(sixDays).map( day => (
                         <ShowTime 
                             /*key={day.hour_data}*/
+                            tamañoDias={tamañoDias}
                             day={day}
                             key={day.hour_data}
                         />
                     ))}
-                
             </div>   
         </div>
     );
--- src/componets/ShowTime.js
+++ src/componets/ShowTime.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import { iconWeather, getHour } from '../helper';
-import styled from 'styled-components';
+import { iconWeather, getHour, iconWind } from '../helper';
 import './ShowTime.css';
 
-const ShowTime = ({day}) => {
+const ShowTime = ({day, tamañoDias}) => {
 
-    const { hour_data, wind_direction, icon, temperature } = day
+    const { hour_data, icon, temperature, icon_wind } = day
     let hour = getHour();
-    const weatherIcon = (hour <= 8 || hour >= 19 ) 
+    const weatherIcon = (hour <= 8 || hour >= 19) 
         ?
-            iconWeather(parseInt(icon))
+            iconWeather(icon)
         :
-            iconWeather(icon);
+            iconWeather(parseInt(icon));
 
-    
+    const windIcon = iconWind(icon_wind);
+    const tamaño = (tamañoDias === 4) ? ("s3") : ("s2");
+
+
     return ( 
-    <div className="col s2">
+    <div className={`col ${tamaño}`}>{/*Modificar columnas movil */}
         <div className="wi-static">
             <p className="hour-data">{hour_data}</p>
-            <p>{icon}</p>
-            <i className={`wi ${weatherIcon}`} />            <div className="a">
+            <i className={`wi ${weatherIcon}`} />            
+            <div className="a">
                 <div className="prueba" style={{height: temperature +20+ 'px'}}>
                     <p className="tem">{temperature}º</p>
                 </div>
-            </div>    
-            <p>{wind_direction}</p>
+            </div>
+            <div>
+                <i className={`wi wi-wind ${windIcon}`} />
+            </div>
         </div>
     </div>
         
